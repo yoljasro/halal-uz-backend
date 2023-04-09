49908fabf73ab3eee11123eda8a8fda04bcd8905
saidaliyevjasur450@gmail.com <saidaliyevjasur450@gmail.com>
2023-04-10 00:16:32 +0500

    g

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,41 +1,38 @@
-const User = require('../models/register');
+const User = require("../models/register");
 const bcrypt = require("bcrypt");
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
 const getUserController = async (req, res) => {
   const { userId } = req.user;
-  const user = await User
-  .findOne({_id: userId})
-  .select('email name');
+  const user = await User.findOne({ _id: userId }).select("email name");
 
   res.status(200).json({ user });
 };
 
 const createUserController = async (req, res) => {
   try {
-    console.log(req.body); 
+    console.log(req.body);
     const { email, password, name } = req.body;
 
     const candidate = await User.findOne({ email });
 
-     
     if (candidate)
-      return res.status(400).json({message: 'Email already exits'});
+      return res.status(400).json({ message: "Email already exits" });
 
     const hashPassword = await bcrypt.hash(password, 12);
 
     const newUser = {
       email,
       password: hashPassword,
-      name
-    }
+      name,
+    };
 
     const user = await new User(newUser).save();
 
-    res.status(201).json({message: 'User succesfully created', data: user});
+    res.status(201).json({ message: "User succesfully created", data: user });
   } catch (e) {
     console.log(e.message);
-    res.status(500).json({message: 'Server error'});
+    res.status(500).json({ message: "Server error" });
   }
 };
 
@@ -44,23 +41,22 @@ const loginController = async (req, res) => {
 
   const user = await User.findOne({ email });
 
-  if (!user)
-    return res.status(404).json({message: 'User not found!'});
+  if (!user) return res.status(404).json({ message: "User not found!" });
 
   const isValidePassword = await bcrypt.compare(password, user.password);
 
   if (!isValidePassword)
-    return res.status(401).json({message: 'Login or password not found '});
+    return res.status(401).json({ message: "Login or password not found " });
 
   const payload = {
-    userId: user._id
+    userId: user._id,
   };
-  const accessToken = jwt.sign(payload, '123456', {expiresIn:'2h'});
-  res.status(200).json({accessToken});
-}
+  const accessToken = jwt.sign(payload, "123456", { expiresIn: "2h" });
+  res.status(200).json({ accessToken });
+};
 
 module.exports = {
   getUserController,
   createUserController,
-  loginController
+  loginController,
 };
